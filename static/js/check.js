58af6f3b85ed09fcc3d4cb5879e2bc45f9f56e1d
HannahSeol <tjfgkssk9472@gmail.com>
2023-10-25 22:46:58 +0900

    hannah commit

--- static/js/check.js
+++ static/js/check.js
@@ -1,82 +1,66 @@
-/*
-function countColors() {
+function checkColors() {
     const preferredColors = 'input[name="preferred"]:checked';
     const selectedPreferredColors = document.querySelectorAll(preferredColors);
-    
-    // 선택된 목록의 갯수 세기
     const preferredColorsCnt = selectedPreferredColors.length;
-    console(preferredColorsCnt);
-}
-*/
 
-function checkColors() {
-    
-    str = "좋아하는 색상 : \n";
+    const dislikedColors = 'input[name="disliked"]:checked';
+    const selectedDislikedColors = document.querySelectorAll(dislikedColors);
+    const dislikedColorsCnt = selectedDislikedColors.length;
 
-    /* 좋아하는 색상 유효성 검사 */
-    /*
-    flag = false;
-    for(i=0; i < selectColors.preferred.length; i++){
-        if(selectColors.preferred[i].checked){
-            str += selectColors.preferred[i].value + "\n";
-            flag = true;
-        }
-    }
-    */
+    //alert(preferredColorsCnt + ',' + dislikedColorsCnt);
 
-    flag = false;
-    if(selectColors.preferred.length == 0) {
-        alert("마음에 드는 색상을 선택해주세요." + selectColors.preferred.length);
-    }
-    else if(selectColors.preferred.length == 1) {
-        alert("마음에 드는 색상을 2개 더 선택해주세요." + selectColors.preferred.length);
-    }
-    else if(selectColors.preferred.length == 2) {
-        alert("마음에 드는 색상을 1개 더 선택해주세요." + selectColors.preferred.length);
-    }
-    else if(selectColors.preferred.length == 3) {
+    //좋아하는 색상 3개 + 안좋아하는 색상 1개 선택 경우
+    while((preferredColorsCnt == 3) && (dislikedColorsCnt == 1)) {
+        //선택한 색상 -> JSON
+        var colorsArray = new Array();
+        var arrayCount = 0;
+
         for(i=0; i < selectColors.preferred.length; i++){
-            if(selectColors.preferred[i].checked){
-                str += selectColors.preferred[i].value + "\n";
+            if(selectColors.preferred[i].checked) {
+                colorsArray[arrayCount] = selectColors.preferred[i].value;
+                arrayCount ++;
             }
-            flag = true;
         }
-    }
-    else {
-        alert("마음에 드는 색상은 3개까지 선택 가능합니다.");
-    }
-    
 
-    str += "안 좋아하는 색상 : \n";
-
-    /* 안 좋아하는 색상 유효성 검사 */
-    /*
-    if(selectColors.disliked.length == 0) {
-        alert("마음에 들지 않는 색상을 선택해주세요.");
-        return false;
-    }
-    else if(selectColors.disliked.length == 1) {
         for(i=0; i < selectColors.disliked.length; i++){
-            if(selectColors.disliked[i].checked){
-                str += selectColors.disliked[i].value + "\n";
+            if(selectColors.disliked[i].checked) {
+                colorsArray[arrayCount] = selectColors.disliked[i].value;
+                arrayCount ++;
             }
         }
+
+        var colorsJson = JSON.stringify(colorsArray);
+        alert(colorsJson);
+
+        return true;
     }
-    else {
-        alert("마음에 들지 않는 색상은 1개까지 선택 가능합니다.");
+
+
+    if(preferredColorsCnt == 0) {
+        alert("마음에 드는 색상을 선택해주세요.");
         return false;
     }
-    */
-    
-    flag = false;
-    if(selectColors.disliked.checked){
-        str += selectColors.disliked.value + "\n";
-        flag = true;
+    else if(preferredColorsCnt == 1) {
+        alert("마음에 드는 색상을 2개 더 선택해주세요.");
+        return false;
     }
-        
-    if(flag == false){
+    else if(preferredColorsCnt == 2) {
+        alert("마음에 드는 색상을 1개 더 선택해주세요.");
+        return false;
+    }
+    else if(preferredColorsCnt > 3) {
+        alert("마음에 드는 색상은 3개까지 선택할 수 있어요.");
+        return false;
+    }
+
+    if(dislikedColorsCnt == 0) {
         alert("마음에 들지 않는 색상을 선택해주세요.");
         return false;
     }
+    else if(preferredColorsCnt > 1) {
+        alert("마음에 들지 않는 색상은 1개까지 선택할 수 있어요.");
+        return false;
+    }
+
     
 }
